685c5cfe873b6985cd6ade7ca37e5cdc9d73810e
Zachary Vanscoit <zachary816@gmail.com>
2018-12-08 17:19:28 -0600

    Added Latex template
    and added code to sound handler

--- Java Simulation/src/main/SoundHandler.java
+++ Java Simulation/src/main/SoundHandler.java
@@ -9,6 +9,7 @@ public class SoundHandler {
 	private Minim minim;
 	private FFT fft;
 	private AudioInput lineIn;
+	private float desiredSpec = 0.02f;
 	
 	public SoundHandler() {
 		minim = new Minim(this);
@@ -16,5 +17,15 @@ public class SoundHandler {
 		lineIn.mute();
 		fft = new FFT(lineIn.bufferSize(), lineIn.sampleRate());
 	}
-
+	
+	public float getFFT() {
+		fft.forward(lineIn.mix);
+		float comb = 0;
+		for (int i=0; i < fft.specSize() * desiredSpec; i++) {
+			comb += fft.getBand(i);
+		}
+		return comb;
+	}
+	
+	
 }
